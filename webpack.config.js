9d078250a120023213a8844db101b276d7d3e738
BengBu-YueZhang <zhangyue_coder@163.com>
2019-03-31 21:36:31 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const HappyPack = require('happypack')
+
+module.exports = {
+  devtool: '#cheap-module-eval-source-map',
+
+  mode: 'development',
+
+  target: 'web',
+
+  entry: {
+    main: path.resolve(__dirname, './index.js')
+  },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true
+  },
+
+  resolve: {
+    extensions: ['.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'happypack/loader?id=js'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HappyPack({
+      id: 'js',
+      threads: 4,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-syntax-dynamic-import'
+            ]
+          }
+        }
+      ]
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, './dist/index.html'),
+      template: path.resolve(__dirname, './public/index.html')
+    })
+  ]
+}
