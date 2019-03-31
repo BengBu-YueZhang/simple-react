b066a1adcfeec33cfc2df8bcac1caf900d1591e5
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-01 00:29:02 +0800

    添加diffChildren，diff方法

--- src/diff/diff.js
+++ src/diff/diff.js
@@ -1,4 +1,38 @@
-export function diff () {
+// It hasn't been implemented yet
+export function diff (
+  dom,
+  root,
+  newVNode,
+  oldVNode,
+  mounts,
+  force
+) {
+  // 删除整个子树
+  if (oldVNode == null || newVNode == null || newVNode.type !== oldVNode.type) {
+    if (!newVNode) return null
+    dom = null
+    oldVNode = {}
+  }
+
+  let newType = newVNode.type
+
+  if (typeof newType === 'function') {
+    // ... Component
+  } else {
+    dom = diffElementNodes(
+      dom,
+      newVNode,
+      oldVNode,
+      mounts
+    )
+  }
+
+  newVNode._dom = dom
+
+  return dom
+}
+
+export function diffElementNodes (dom, newVNode, oldVNode) {
 }
 
 export function runDidMount () {
