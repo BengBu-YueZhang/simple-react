19fbe1d3cde4972ee9497a4efcb78c58ba7e3b89
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-03 21:40:23 +0800

    更新例子

--- example/index.js
+++ example/index.js
@@ -1,9 +1,14 @@
-import { h, render } from '../src'
+import { h, render, Component } from '../src'
+
+class HelloWorld extends Component {
+  render () {
+    return (
+      <h1 id="title" style="color: blue">hello world</h1>
+    )
+  }
+}
 
 render(
-  <div>
-    <h1 id="title" style="color: blue">hello world</h1>
-    <h1 id="title" style="color: red">hello world</h1>
-  </div>,
+  <HelloWorld/>,
   document.getElementById('app')
 )
