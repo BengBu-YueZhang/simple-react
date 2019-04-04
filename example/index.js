cc878d817ef9a3ae747fb7adfcbc70cde9578236
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-04 15:23:57 +0800

    修改例子

--- example/index.js
+++ example/index.js
@@ -1,11 +1,33 @@
 import { h, render, Component } from '../src'
 
 class HelloWorld extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      msg1: 'msg1',
+      msg2: 'msg2'
+    }
+  }
+
+  componentWillMount () {
+    this.setState({
+      msg1: 'componentWillMount change'
+    })
+  }
+
+  handleClick () {
+    this.setState({
+      msg2: 'handleClick'
+    })
+  }
+
   render () {
     return (
       <div>
-        <h1 id="title" style="color: red">hello world</h1>
-        <h1 id="title" style="color: blue">hello world</h1>
+        <h1 id="title" style="color: red">{ this.state.msg1 }</h1>
+        <h1 id="title" style="color: blue">{ this.state.msg2 }</h1>
+        <button onClick={this.handleClick.bind(this)}>点击</button>
       </div>
     )
   }
