cb8c20fc538f5991d082ee4dab6f51156c195375
BengBu-YueZhang <zhangyue_coder@163.com>
2019-04-07 13:09:54 +0800

    reset

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { createElement, createElement as h } from './create-element'
-export { render } from './render'
-export { Component } from './components'
--- src/util.js
+++
@@ -1,6 +0,0 @@
-export function assign(obj, props) {
-	if (props) {
-		return Object.assign(obj, props)
-	}
-	return obj
-}
